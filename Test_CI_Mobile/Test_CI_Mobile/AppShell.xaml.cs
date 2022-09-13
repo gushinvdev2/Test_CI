9cea98aef7ed34c5872719b58d6c84568f22c668
VadimGushin <vadim.gushin@a-dev.com>
2022-09-13 19:41:06 +0300

    Changed develop

--- Test_CI_Mobile/Test_CI_Mobile/AppShell.xaml.cs
+++ Test_CI_Mobile/Test_CI_Mobile/AppShell.xaml.cs
@@ -11,6 +11,7 @@ namespace Test_CI_Mobile
         public AppShell()
         {
             InitializeComponent();
+
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
